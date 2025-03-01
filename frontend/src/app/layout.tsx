5c5a9d03e7e1431ac61afe3928bc63070a7eaf53
ACRnm <contact@aidenr.dev>
2025-03-01 22:59:11 +1100

    Update layout styles in RootLayout and change heading color in Home component

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 }>) {
   return (
       <html lang="en" suppressHydrationWarning>
-          <body className={`${GeistSans.className} antialiased`}>
+          <body>
               <ThemeProvider
                   attribute="class"
                   defaultTheme="system"
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           <Meteors />
           <div className="relative flex min-h-screen flex-col items-center justify-between p-4 sm:p-12 md:p-24">
               <div className="w-full max-w-4xl flex flex-col items-center space-y-8">
-                  <h1 className="text-4xl font-bold text-white">DateTime</h1>
+                  <h1 className="text-4xl font-bold text-foreground">DateTime</h1>
           <ConverterForm />
               </div>
           </div>
